52a9121d07522bd733935d78000aa8b94e80ab08
audetv <algsound@yandex.ru>
2025-10-14 21:29:06 +0300

    Создали инраструктурный UUIDGenerator

---
+++ backend/internal/infrastructure/id/generator.go
@@ -0,0 +1,27 @@
+package id
+
+import (
+	"fmt"
+
+	// Импортируем domain
+	"github.com/google/uuid"
+)
+
+// UUIDGenerator реализует domain.IDGenerator используя google/uuid
+type UUIDGenerator struct{}
+
+func NewUUIDGenerator() *UUIDGenerator {
+	return &UUIDGenerator{}
+}
+
+func (g *UUIDGenerator) GenerateID() string {
+	return uuid.New().String()
+}
+
+func (g *UUIDGenerator) GenerateMessageID() string {
+	return fmt.Sprintf("<%s@urms.local>", uuid.New().String())
+}
+
+func (g *UUIDGenerator) GenerateThreadID() string {
+	return fmt.Sprintf("thread_%s", uuid.New().String())
+}
